ed5f43925f32ecebe9ecda0ba9b3dd8214acda36
venurawarnasooriya <venura@ashogu.com>
2024-11-30 13:28:58 +0530

    api keys generated and stored after encryption

--- Helpers/Common.py
+++ Helpers/Common.py
@@ -1,7 +1,9 @@
 
+import os
 from custom_types.UserDetails import APIKeys, UserDetails
 from utils.password_utils import hash_password
 from google.cloud.firestore import Client
+from cryptography.fernet import Fernet
 
 def convert_json_to_user_details(data_as_json) -> UserDetails:
     return UserDetails(
@@ -17,8 +19,8 @@ def convert_json_to_user_details(data_as_json) -> UserDetails:
         description = data_as_json.get("description"),
         choosen_llm = data_as_json.get('choosen_llm'),
         api_keys = APIKeys(
-            openai_api_key= data_as_json.get('openai_api_key'),
-            llama_api_key=data_as_json.get('llama_api_key')
+            openai_api_key = encrypt_data(data_as_json.get('openai_api_key')),
+            llama_api_key = encrypt_data(data_as_json.get('llama_api_key'))
         ),
     )
 
@@ -26,3 +28,29 @@ def is_email_exists(db: Client, collection:str , email: str):
     user_data = db.collection(collection).where('email', '==', email).limit(1)
     return any(user_data.stream())
 
+def encrypt_data(value: str) -> (str):
+    if not value:
+        return ""
+    try:
+        key = os.environ.get('FERNET_KEY', '')
+        fernet = Fernet(key.encode())
+        encrypted_data = fernet.encrypt(value.encode())
+        return encrypted_data.decode()
+    
+    except Exception as e:
+        raise Exception("Server Error Occured while encrypting the keys")
+
+
+
+def decrypt_data(encrypted_value: str) -> str:
+    if not encrypt_data:
+        return ""
+    
+    try:
+        key = os.environ.get('FERNET_KEY', '')
+        fernet = Fernet(key.encode())
+        decrupted_data = fernet.encrypt(encrypted_value.encode())
+        return decrupted_data.decode()
+    
+    except Exception as e:
+        raise Exception("Server Error Occured while encrypting the keys")
